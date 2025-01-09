7556dbc72513546f61d2a149808570be09a9e918
Tahmurath <hamidreza.tahmooresi@gmail.com>
2025-01-09 17:05:55 +0330

    fix static build problem

--- src/app/(panel)/department/page.tsx
+++ src/app/(panel)/department/page.tsx
@@ -26,212 +26,216 @@ import {
 import {cn} from "@/lib/utils";
 import { Check, ChevronsUpDown } from "lucide-react"
 
+interface DepType {
+    value: number;
+    label: string;
+}
+
+const token = isExpiredJwt()
+
+const FormSchema = z.object({
+    title: z.string().min(3, {
+        message: "Title must be at least 3 characters.",
+    }),
+    departmenttypeid: z.preprocess((value) => Number(value), z.number({
+        message: "DepartmentTypeID must be a number.",
+    })),
+    parentid: z.preprocess((value) => Number(value), z.number({
+        message: "ParentID must be a number.",
+    })),
+
+});
+
+
+function InputForm({
+                       title = "",
+                       departmenttypeid,
+                       parentid,
+                   }: {
+    title?: string; // می‌تواند خالی باشد
+    departmenttypeid?: number; // می‌تواند خالی باشد
+    parentid?: number; // می‌تواند خالی باشد
+}) {
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            title: title,
+            departmenttypeid: departmenttypeid,
+            parentid: parentid,
+        },
+    })
+
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+    const [depTypes, setDepTypes] = useState<DepType[]>([]);
+    const [query, setQuery] = useState("");
+
+    useEffect(() => {
+        async function fetchDepTypes(searchQuery = "") {
+            try {
+                const response = await fetch(`http://localhost:8080/api/v1/department/list?query=${searchQuery}`);
+                const data = await response.json();
+                setDepTypes(data);
+            } catch (error) {
+                console.error('Error fetching depTypes:', error);
+            }
+        }
+
+        fetchDepTypes(query); // دریافت داده‌ها بر اساس عبارت جستجو
+    }, [query]);
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        //alert(JSON.stringify(data, null, 2))
+        toast({
+            title: "You submitted the following values:",
+            description: (
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+              <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+            </pre>
+            ),
+        })
+        //const token = getTokenFromCookie()
+        fetch("http://127.0.0.1:8080/api/v1/department/new", {
+            method: "POST",
+            body: JSON.stringify({
+                title: data.title,
+                departmenttypeid: Number(data.departmenttypeid),
+                parentid: Number(data.parentid),
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`, // ست کردن هدر Authorization
+            }
+        })
+            .then((response) => response.json())
+            .then((json) =>
+            {
+                console.log(json);
+                // saveTokenToCookie(json.token)
+                // router.push('/dashboard');
+            });
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="block text-sm/6 font-medium text-gray-900">Title</FormLabel>
+
+                            <FormControl>
+                                <Input placeholder="Title" {...field}/>
+                            </FormControl>
+                            <FormDescription>
+                                Department title
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="departmenttypeid"
+                    render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                            <FormLabel>departmenttypeid</FormLabel>
+                            <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+                                <PopoverTrigger asChild>
+                                    <FormControl>
+                                        <Button
+                                            variant="outline"
+                                            role="combobox"
+                                            className={cn(
+                                                "w-[200px] justify-between",
+                                                !field.value && "text-muted-foreground"
+                                            )}
+                                            onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+                                        >
+                                            {field.value
+                                                ? depTypes.find(
+                                                    (departmenttypeid) => departmenttypeid.value === field.value
+                                                )?.label
+                                                : "Select department type"}
+                                            <ChevronsUpDown className="opacity-50" />
+                                        </Button>
+                                    </FormControl>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-[200px] p-0">
+                                    <Command>
+                                        <CommandInput
+                                            placeholder="Search framework..."
+                                            className="h-9"
+                                            onValueChange={setQuery}
+                                        />
+                                        <CommandList>
+                                            <CommandEmpty>No framework found.</CommandEmpty>
+                                            <CommandGroup>
+                                                {depTypes.map((departmenttypeid) => (
+                                                    <CommandItem
+                                                        value={departmenttypeid.label}
+                                                        key={departmenttypeid.value}
+                                                        onSelect={() => {
+                                                            form.setValue("departmenttypeid", departmenttypeid.value)
+                                                            setIsPopoverOpen(false);
+                                                        }}
+                                                    >
+                                                        {departmenttypeid.label}
+                                                        <Check
+                                                            className={cn(
+                                                                "ml-auto",
+                                                                departmenttypeid.value === field.value
+                                                                    ? "opacity-100"
+                                                                    : "opacity-0"
+                                                            )}
+                                                        />
+                                                    </CommandItem>
+                                                ))}
+                                            </CommandGroup>
+                                        </CommandList>
+                                    </Command>
+                                </PopoverContent>
+                            </Popover>
+                            <FormDescription>
+                                This is the department type that will be used in the dashboard.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+
+                <FormField
+                    control={form.control}
+                    name="parentid"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel className="block text-sm/6 font-medium text-gray-900">parentid</FormLabel>
+                            <FormControl>
+                                <Input placeholder="parentid" {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                parentid
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    )
+}
 
 export default function Home() {
 
-    const token = isExpiredJwt()
-
-
-
-    const FormSchema = z.object({
-        title: z.string().min(3, {
-            message: "Title must be at least 3 characters.",
-        }),
-        departmenttypeid: z.preprocess((value) => Number(value), z.number({
-            message: "DepartmentTypeID must be a number.",
-        })),
-        parentid: z.preprocess((value) => Number(value), z.number({
-            message: "ParentID must be a number.",
-        })),
-
-    });
 
     const [error, setError] = useState<any>(null);
 
-
-    function InputForm({
-                           title = "",
-                           departmenttypeid,
-                           parentid,
-                       }: {
-        title?: string; // می‌تواند خالی باشد
-        departmenttypeid?: number; // می‌تواند خالی باشد
-        parentid?: number; // می‌تواند خالی باشد
-    }) {
-
-        const form = useForm<z.infer<typeof FormSchema>>({
-            resolver: zodResolver(FormSchema),
-            defaultValues: {
-                title: title,
-                departmenttypeid: departmenttypeid,
-                parentid: parentid,
-            },
-        })
-
-        const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-        const [languages, setLanguages] = useState([]);
-        const [query, setQuery] = useState("");
-
-        useEffect(() => {
-            async function fetchLanguages(searchQuery = "") {
-                try {
-                    const response = await fetch(`http://localhost:8080/api/v1/department/list?query=${searchQuery}`);
-                    const data = await response.json();
-                    setLanguages(data);
-                } catch (error) {
-                    console.error('Error fetching languages:', error);
-                }
-            }
-
-            fetchLanguages(query); // دریافت داده‌ها بر اساس عبارت جستجو
-        }, [query]);
-
-        function onSubmit(data: z.infer<typeof FormSchema>) {
-            //alert(JSON.stringify(data, null, 2))
-            toast({
-                title: "You submitted the following values:",
-                description: (
-                    <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-              <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-            </pre>
-                ),
-            })
-            //const token = getTokenFromCookie()
-            fetch("http://127.0.0.1:8080/api/v1/department/new", {
-                method: "POST",
-                body: JSON.stringify({
-                    title: data.title,
-                    departmenttypeid: Number(data.departmenttypeid),
-                    parentid: Number(data.parentid),
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`, // ست کردن هدر Authorization
-                }
-            })
-                .then((response) => response.json())
-                .then((json) =>
-                {
-                    console.log(json);
-                    // saveTokenToCookie(json.token)
-                    // router.push('/dashboard');
-                });
-        }
-
-        return (
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-                    <FormField
-                        control={form.control}
-                        name="title"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel className="block text-sm/6 font-medium text-gray-900">Title</FormLabel>
-
-                                <FormControl>
-                                    <Input placeholder="Title" {...field}/>
-                                </FormControl>
-                                <FormDescription>
-                                    Department title
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-
-                    <FormField
-                        control={form.control}
-                        name="departmenttypeid"
-                        render={({ field }) => (
-                            <FormItem className="flex flex-col">
-                                <FormLabel>departmenttypeid</FormLabel>
-                                <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
-                                    <PopoverTrigger asChild>
-                                        <FormControl>
-                                            <Button
-                                                variant="outline"
-                                                role="combobox"
-                                                className={cn(
-                                                    "w-[200px] justify-between",
-                                                    !field.value && "text-muted-foreground"
-                                                )}
-                                                onClick={() => setIsPopoverOpen(!isPopoverOpen)}
-                                            >
-                                                {field.value
-                                                    ? languages.find(
-                                                        (departmenttypeid) => departmenttypeid.value === field.value
-                                                    )?.label
-                                                    : "Select language"}
-                                                <ChevronsUpDown className="opacity-50" />
-                                            </Button>
-                                        </FormControl>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-[200px] p-0">
-                                        <Command>
-                                            <CommandInput
-                                                placeholder="Search framework..."
-                                                className="h-9"
-                                                onValueChange={setQuery}
-                                            />
-                                            <CommandList>
-                                                <CommandEmpty>No framework found.</CommandEmpty>
-                                                <CommandGroup>
-                                                    {languages.map((departmenttypeid) => (
-                                                        <CommandItem
-                                                            value={departmenttypeid.label}
-                                                            key={departmenttypeid.value}
-                                                            onSelect={() => {
-                                                                form.setValue("departmenttypeid", departmenttypeid.value)
-                                                                setIsPopoverOpen(false);
-                                                            }}
-                                                        >
-                                                            {departmenttypeid.label}
-                                                            <Check
-                                                                className={cn(
-                                                                    "ml-auto",
-                                                                    departmenttypeid.value === field.value
-                                                                        ? "opacity-100"
-                                                                        : "opacity-0"
-                                                                )}
-                                                            />
-                                                        </CommandItem>
-                                                    ))}
-                                                </CommandGroup>
-                                            </CommandList>
-                                        </Command>
-                                    </PopoverContent>
-                                </Popover>
-                                <FormDescription>
-                                    This is the language that will be used in the dashboard.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-
-
-                    <FormField
-                        control={form.control}
-                        name="parentid"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel className="block text-sm/6 font-medium text-gray-900">parentid</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="parentid" {...field}
-                                    />
-                                </FormControl>
-                                <FormDescription>
-                                    parentid
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit">Submit</Button>
-                </form>
-            </Form>
-        )
-    }
 
 
   return (
@@ -249,8 +253,6 @@ export default function Home() {
                   <InputForm title={""} departmenttypeid={undefined} parentid={undefined}></InputForm>
               </div>
           </div>
-
-
       </div>
   );
 }
